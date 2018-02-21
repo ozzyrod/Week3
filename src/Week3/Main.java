d451bbd431b005352845886f790fd746e5f1d790
Ozzy Rodriguez <ozzy@ozzyrodriguez.com>
2018-02-20 18:01:50 -0800

    Initial commit

---
+++ src/Week3/Main.java
@@ -0,0 +1,14 @@
+/*
+ *
+ * Project: Week3
+ * Author:  Osbardo Rodriguez
+ * Date:    2/20/18
+ */
+package Week3;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
